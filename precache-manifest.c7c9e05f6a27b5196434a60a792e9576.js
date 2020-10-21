000d6b5800bf4d8de37057053e322ef035d8699d
Christina <christinalu42@gmail.com>
2020-10-21 08:49:17 -0600

    Updates

---
+++ precache-manifest.c7c9e05f6a27b5196434a60a792e9576.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41827898cff75f42f43207e727e5b472",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "d5ad886f9d34fba839f9",
+    "url": "/minesweeper/static/css/main.45b60545.chunk.css"
+  },
+  {
+    "revision": "f1fd85ec637f6098c72c",
+    "url": "/minesweeper/static/js/2.722e3c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.722e3c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ad886f9d34fba839f9",
+    "url": "/minesweeper/static/js/main.d121d8ca.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  },
+  {
+    "revision": "dd28dafe0cadf724b7cf6593fc235487",
+    "url": "/minesweeper/static/media/2.dd28dafe.png"
+  },
+  {
+    "revision": "2c4ef9e395e48fa29a48f4157554a664",
+    "url": "/minesweeper/static/media/3.2c4ef9e3.png"
+  },
+  {
+    "revision": "896832e0e65fb52c8e4d47f5f15d6fe0",
+    "url": "/minesweeper/static/media/4.896832e0.png"
+  },
+  {
+    "revision": "93881214cc3d48fdef55fd98b953811c",
+    "url": "/minesweeper/static/media/5.93881214.png"
+  },
+  {
+    "revision": "6b02fd2fd89c3582e8b83157aa0a8671",
+    "url": "/minesweeper/static/media/6.6b02fd2f.png"
+  },
+  {
+    "revision": "1ad240f57a5ebaa8cb20950defcf2bb7",
+    "url": "/minesweeper/static/media/7.1ad240f5.png"
+  },
+  {
+    "revision": "7aeabb30e2d1cad13642d075b667f4fe",
+    "url": "/minesweeper/static/media/8.7aeabb30.png"
+  },
+  {
+    "revision": "ae6cbdbffbcfe0d17cd28350919b9801",
+    "url": "/minesweeper/static/media/correction.ae6cbdbf.png"
+  },
+  {
+    "revision": "c61c20acd9716483e1dd05fae0ad0d84",
+    "url": "/minesweeper/static/media/flag.c61c20ac.png"
+  },
+  {
+    "revision": "cbd5fc8e291a319d6640586c4cdf4a85",
+    "url": "/minesweeper/static/media/mine.cbd5fc8e.png"
+  },
+  {
+    "revision": "acf89a48045e2c9326bf3c5e4a84bccd",
+    "url": "/minesweeper/static/media/timer.acf89a48.png"
+  }
+]);
